dc28cbc18b9604b1677217cc76f3886637886e17
Karim Tarek <126930962+karimtmagdy@users.noreply.github.com>
2024-05-18 18:50:22 +0300

    Update Grid.jsx

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,2 +1,12 @@
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+import { cva } from "class-variance-authority";
 
-const Grid =()=>{}
+const Grid =({,,,,,,,,,,,})=>{
+const styles=cva([],{
+variants:{
+
+  },
+ })
+}
+export default Grid;
