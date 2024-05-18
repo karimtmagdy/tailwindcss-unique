01e481f7f8f62d94e36f6929a705c6eff7eee610
Karim Tarek <126930962+karimtmagdy@users.noreply.github.com>
2024-05-18 19:13:31 +0300

    Update Grid.jsx

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -3,9 +3,16 @@ import { twMerge } from "tailwind-merge";
 import { cva } from "class-variance-authority";
 
 const Grid =({,,,,,,,,,,,})=>{
-const styles=cva([],{
+const styles=cva(['grid',''],{
 variants:{
-
+xs:{'grid-cols-1'},
+responsive:[
+ xs:{'grid-cols-2'},
+ sm:{'grid-cols-2'},
+ md:{'gris-cols-3'},
+ lg:{'grid-cols-4'},
+ xl:{'grid-cols-5'},
+],
   },
  })
 
